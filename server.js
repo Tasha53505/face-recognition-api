d19c812811bf163d67a4cc371f909e8460b0d78c
Tasha <ncarr63@gmail.com>
2022-01-27 02:42:36 +1300

    The profile isn't working need to fix
    Fix the profile with Postman

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const database = {
 
 // Testing ---------------------
 app.get('/', (req, res) => {
-        res.send('This is working')
+        res.send(database.users);
     })
     // -----------------------
 
@@ -45,15 +45,39 @@ app.post('/signin', (req, res) => {
     })
     // ----------Registering--------------------
 
-app.post('/regisiter', (req, res) => {
-    req.body
+app.post('/register', (req, res) => {
+        const { email, name, password } = req.body;
+        database.users.push({
+                id: '125',
+                name: name,
+                email: email,
+                password: password,
+                entries: 0,
+                joined: new Date()
+            })
+            // This grabs the last user.
+        res.json(database.users[database.users.length - 1])
+    })
+    // --------------Profile---------------------
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json("Not found..");
+    }
 })
 
 
 
-// -------------------------
 
+// -------------------------------
 
 // Listening to port 3000
 app.listen(3000, () => {
