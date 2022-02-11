8747b212ee2b358b4f97e1c2358baf3d9ddeb174
Tasha <ncarr63@gmail.com>
2022-02-12 06:19:46 +1300

    hope all is fixed now

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
 const db = knex({
     client: 'pg',
     connection: {
         host: 'postgresql-fitted-76115',
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
+        ssl: true
     }
 });
 
