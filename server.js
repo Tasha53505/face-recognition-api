7bb213f7d7b26c21d740176cae46c09806943a37
Tasha <ncarr63@gmail.com>
2022-02-12 04:52:24 +1300

    trying to connect it all together

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-fitted-76115',
         user: 'postgres',
         password: 'Crumpet63',
         database: 'face-recognition-ai'
